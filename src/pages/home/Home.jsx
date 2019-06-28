157c3db2ea30d5e25d29a044a1e1f046a78dea72
Sriram <DhiraSri@Scholastic.com>
2019-06-28 15:12:39 -0400

    Load more results fix

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,18 +9,42 @@ class Home extends React.Component {
     super(props);
     this.state = {
       number: 0,
-      pokeArr: []
+      pokeArr: [],
+      randomized:false
     };
   }
 
   componentDidMount() {
     let initArr = Array.from(Array(24).keys(), x => (x + 1))
     this.setState({ "pokeArr": initArr })
+    document.addEventListener('scroll', this.trackScrolling);
   }
+
+  componentWillUnmount() {
+    document.removeEventListener('scroll', this.trackScrolling);
+  }
+
+  isBottom(el) {
+    return el.getBoundingClientRect().bottom - 400 <= window.innerHeight;
+  }
+
+  trackScrolling = () => {
+    const wrappedElement = document.body;
+    if (this.isBottom(wrappedElement)) {
+      if(!this.state.randomized){
+      let initArr = Array.from(Array(this.state.pokeArr.length + 24).keys(), x => (x + 1))
+      this.setState({ "pokeArr": initArr })
+    }
+    else{
+    let arr = Array.from({ length: 24 }, () => Math.floor(Math.random() * 808));
+    this.setState({ "pokeArr": this.state.pokeArr.concat(arr)})
+    }
+    }
+  };
 
   calculateRand = (e)=> {
     let arr = Array.from({ length: 24 }, () => Math.floor(Math.random() * 808));
-    this.setState({ "pokeArr": arr })
+    this.setState({ "pokeArr": arr , randomized : true})
   }
 
   SearchData(params) {
@@ -40,7 +64,6 @@ class Home extends React.Component {
             </Fab>
           </Grid>
           <Grid container item xs={6} spacing={4}>
-          
           </Grid>
         </Grid>
         <Results pokeArr={this.state.pokeArr} />
