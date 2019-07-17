c2e6ca32b10fa81bfc5ff8d554430b3a9172adaf
Sriram <DhiraSri@Scholastic.com>
2019-07-17 16:00:40 -0400

    Added code splitting and react suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Home from './pages/home/Home';
 import { Route, Switch } from 'react-router-dom';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 import { grey } from '@material-ui/core/colors';
-import Description from './pages/description/Description';
-import Listing from './pages/listing/Listing';
 import Menubar from './common/menu/Menubar';
-import FourZeroFour from './pages/404/FourZeroFour';
+const Home = React.lazy(() => import('./pages/home/Home'));
+const Description = React.lazy(() => import('./pages/description/Description'));
+const Listing = React.lazy(() => import('./pages/listing/Listing'));
+const FourZeroFour = React.lazy(() => import('./pages/404/FourZeroFour'));
 
 const theme = createMuiTheme({
 	palette: {
@@ -26,19 +26,19 @@ class App extends Component {
 
 	render() {
 		return (
-				<ThemeProvider theme={theme}>
-							<React.Fragment>
-								<Menubar/>
-								<div className="main-container">
-								<Switch>
-									<Route exact path="/" component={Home} />
-									<Route exact path="/results/:id" component={Listing} />
-									<Route exact path="/description/:id" component={Description} />
-									<Route component={FourZeroFour} />
-								</Switch>
-								</div>
-							</React.Fragment>
-				</ThemeProvider>
+			<ThemeProvider theme={theme}>
+				<Menubar />
+				<div className="main-container">
+					<React.Suspense fallback={<div>Loading...</div>}>
+						<Switch>
+							<Route exact path="/" render={(props) => <Home {...props} />} />
+							<Route exact path="/results/:id" render={(props) => <Listing {...props} />} />
+							<Route exact path="/description/:id" render={(props) => <Description {...props} />} />
+							<Route render={(props) => <FourZeroFour {...props} />} />
+						</Switch>
+					</React.Suspense>
+				</div>
+			</ThemeProvider>
 		);
 	}
 }
